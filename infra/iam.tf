672ef7a4bf269f6cfd953a99357910db542320b5
Mahfuzul Haque <Mahfuzul.Haque@gmail.com>
2026-03-09 15:49:36 +1100

    infra: allow unauthenticated cloud run access

--- infra/iam.tf
+++ infra/iam.tf
@@ -9,3 +9,16 @@ resource "google_project_iam_member" "cloud_run_metric_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.cloud_run.email}"
 }
+
+resource "google_cloud_run_v2_service_iam_member" "public_invoker" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_run_v2_service.api.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+
+  depends_on = [
+    google_project_service.required,
+    google_cloud_run_v2_service.api
+  ]
+}
